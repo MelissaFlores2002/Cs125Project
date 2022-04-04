303cea5bde71f3cae6dc3804a7d5f0130e039030
DIFABIOH <92065970+DIFABIOH@users.noreply.github.com>
2022-04-04 04:45:39 -0700

    Add files via upload

---
+++ damageFunction.c
@@ -0,0 +1,11 @@
+int damageFunction(int weapon){
+	if (weapon ==1)
+		damage=3;
+	if else (weapon ==2)
+		damage=4;
+	if else ((weapon ==3) || (weapon ==5))
+		damage=1;
+	if else ((weapon ==4) || (weapon ==6))
+		damage = 2;
+	return damage;
+}
