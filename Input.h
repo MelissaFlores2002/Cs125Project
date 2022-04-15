bb179acf721efc90dd5cc1d8c2f7319c7ad0d608
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 13:15:02 -0700

    Update Input.h

--- Input.h
+++ Input.h
@@ -1,3 +1,6 @@
+//Melissa Flores and Hunter Difabio
+#include <stdio.h>
+
 //defining a structure which contains the user's inputs for usage in the errorCheck function
 typedef struct inputs {
     int errorCheck;
