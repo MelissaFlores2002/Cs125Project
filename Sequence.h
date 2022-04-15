e34b4a1939ab4c32bbf6604ac1c8d4086e3faa67
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 13:15:25 -0700

    Update Sequence.h

--- Sequence.h
+++ Sequence.h
@@ -1,3 +1,6 @@
+//Melissa Flores and Hunter Difabio
+#include <stdio.h>
+
 //defining a structure which takes into account location and success of the attack
 typedef struct combatInputs{
 		int target;
