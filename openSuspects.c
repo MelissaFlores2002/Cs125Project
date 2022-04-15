f0854a4be3b5732dbc770925cdde0af95038ddde
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 10:13:32 -0700

    Create openSuspects.c

---
+++ openSuspects.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//this function opens a .txt file for reading which contains the list of suspects
+void openSuspects() {
+  //creating a string variable which is capable of housing an incredibly large number of characters
+  char str[2000000];
+  int x;
+
+  FILE* f1 = fopen("suspects.txt", "r");
+  if (f1==NULL){
+    printf("ERROR opening file\n");
+    return;
+  }
+  //prints 8 lines from the txt file
+  for (x = 0; x < 8; x++) {
+    fgets(str, 2000000, f1);
+    printf("%s", str);
+  }
+  fclose(f1);
+}
