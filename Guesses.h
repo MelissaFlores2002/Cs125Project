d128b397df11fbda464b9ac5386cfdeb03db9b77
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 13:14:38 -0700

    Update Guesses.h

--- Guesses.h
+++ Guesses.h
@@ -1,3 +1,6 @@
+//Melissa Flores and Hunter Difabio
+#include <stdio.h>
+
 //defining a structure which contains the user's guess for the murderer, weapon, and room
 typedef struct guesses {
   int murderer;
