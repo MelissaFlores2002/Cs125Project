1bdbe82df8d69abcf69fa443115816529037e77d
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 13:18:00 -0700

    Update explore.h

--- explore.h
+++ explore.h
@@ -1,3 +1,4 @@
+//Melissa Flores and Hunter Difabio
 #include <stdio.h>
 //what the user gets to explore for the rooms 
 int explore(int room, int roomreal, int* roomsExploredStored, int* roomsExplored);
