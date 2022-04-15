6544ef0a3116a74a89e1c63542cc8be09cc9f375
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 13:18:52 -0700

    Update findWeaponName.c

--- findWeaponName.c
+++ findWeaponName.c
@@ -1,3 +1,4 @@
+//Melissa Flores and Hunter Difabio
 #include <stdio.h>
 void findWeaponName(int weaponNumber, char* weaponName){
   int loopCounter, lengthLine, lengthWeaponName, numberOfSpacesInString=0;
