51388ea6724c5499532921951e996229b0c14747
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 13:16:00 -0700

    Update beenInRoomCheck.h

--- beenInRoomCheck.h
+++ beenInRoomCheck.h
@@ -1,3 +1,4 @@
+//Melissa Flores and Hunter Difabio
 #include <stdio.h>
 //checking if the user has travled to a room already
 bool beenInRoomCheck(int room, int* roomsExplored, int* numberOfRooms);
