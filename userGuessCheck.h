dc71542a4b5e18197363711e7a7269d0fa653b2c
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 12:09:19 -0700

    Create userGuessCheck.h

---
+++ userGuessCheck.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+//All of the functions we use have been put into sepreate .c files to make our main code cleaner
+//checks whether the users guess is correct or wrong, then ammends it to a file
+int userGuessCheck(int guess, int real);
