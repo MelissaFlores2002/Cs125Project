c9ba540a7cb69f0c22ddb47ebdcf2d0456425d2c
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 10:37:44 -0700

    Create AItargetFunction.c

---
+++ AItargetFunction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int AItargetFunction(){
+//rolls randomly to determine position of attack, it is more likely to attack higher positions due to the increased critical strike chance
+  int roll, target;
+  roll=rand()%13;
+  if (roll <= 2)
+        target=1;
+  else if (roll <= 6)
+        target=2;
+  else if (roll <=12)
+        target=3;
+  return target;
+}
