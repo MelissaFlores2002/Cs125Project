4549040ac02b925c5ef46aadb89e75e891215879
Melissa <41021321+MelissaFlores2002@users.noreply.github.com>
2022-04-15 10:12:06 -0700

    Create openWeapons.c

---
+++ openWeapons.c
@@ -0,0 +1,19 @@
+
+//opens a .txt file for reading which contains the list of weapons
+void openWeapons() {
+  //creating a string variable which is capable of housing an incredibly large number of characters
+  char str[2000000];
+  int x;
+
+  FILE* f1 = fopen("weapons.txt", "r");
+  if (f1==NULL){
+    printf("ERROR opening file\n");
+    return;
+  }
+  //prints 8 lines from the txt file
+  for (x = 0; x < 8; x++) {
+    fgets(str, 2000000, f1);
+    printf("%s", str);
+  }
+  fclose(f1);
+}
